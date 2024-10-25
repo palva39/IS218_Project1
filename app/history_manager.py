36f2dfa1df66df642ea042f09a5d31cfb493ed46
palva39 <paa39@njit.edu>
2024-10-25 16:59:57 -0400

    updated history manger to use the facade

--- app/history_manager.py
+++ app/history_manager.py
@@ -1,32 +1,23 @@
-# app/history_manager.py
-import pandas as pd
-import os
+from app.pandas_facade import PandasFacade
 
 class HistoryManager:
     def __init__(self, history_file='data/calculation_history.csv'):
         self.history_file = history_file
-        self.history = pd.DataFrame(columns=['operation', 'a', 'b', 'result'])
-
-        if os.path.exists(self.history_file):
-            self.load_history()
+        self.history = PandasFacade.load_csv(self.history_file)
 
     def load_history(self):
-        self.history = pd.read_csv(self.history_file)
+        self.history = PandasFacade.load_csv(self.history_file)
 
     def save_history(self):
-        self.history.to_csv(self.history_file, index=False)
+        PandasFacade.save_csv(self.history, self.history_file)
 
     def record(self, record):
-        # Create a DataFrame from the record
-        new_record = pd.DataFrame([record])
-
-        # Check if the new_record is not empty before concatenating
-        if not new_record.empty and not new_record.isna().all().all():
-            self.history = pd.concat([self.history, new_record], ignore_index=True)
+        if all(key in record for key in ['operation', 'a', 'b', 'result']) and not any(pd.isna(value) for value in record.values()):
+            self.history = PandasFacade.concat_dataframes(self.history, record)
             self.save_history()
 
     def get_history(self):
-        return self.history.to_string(index=False)
+        return PandasFacade.get_dataframe_string(self.history)
 
     def clear_history(self):
         self.history = pd.DataFrame(columns=['operation', 'a', 'b', 'result'])
