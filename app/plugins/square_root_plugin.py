4fd8a74912f2d7630452cfbef38d643d9d81e005
palva39 <paa39@njit.edu>
2024-10-25 18:46:29 -0400

    adjusted plugin name

--- app/plugins/square_root_plugin.py
+++
@@ -1,7 +0,0 @@
-
-import math
-
-def square_root(number):
-    if number < 0:
-        raise ValueError("Cannot calculate the square root of a negative number.")
-    return math.sqrt(float(number))
